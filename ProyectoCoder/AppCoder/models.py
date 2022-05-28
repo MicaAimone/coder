424ec4229dfbe1a28748fd0cba9106de9182fa4f
MicaAimone <mica.aimone@gmail.com>
2022-05-28 20:07:07 -0300

    app

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+        nombre = models.CharField(max_length=40)
+        camada = models.IntegerField()
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=40)
+
+class Entregable(models.Model):
+    nombre =models.CharField(max_length=30)
+    FechaDeEntrega = models.DateField()
+    entregado = models.BooleanField()
--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from AppCoder.models import Curso
 # Create your views here.
+
+def curso(self):
+    curso = Curso(nombre = "Desarrollo web", camada ="1234")
+    curso.save()
+    documentoDeTexto = f"----->Curso: {curso.nombre},  Camada: {curso.camada}"
+
+    return HttpResponse(documentoDeTexto)
